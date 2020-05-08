8ae695c9209cccbb4ec734d9b54f5138c75bb62c
S.Z. Zheng <s.z.zheng@foxmail.com>
2020-05-09 00:37:18 +0800

    v1.1

--- ImageCoordinateTransformation/ImageCoordinateTransformation/ImgRotation.cpp
+++ ImageCoordinateTransformation/ImageCoordinateTransformation/ImgRotation.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int w = 256;
 int h = 256;
 //int wDst = sqrt((double)w * w + (double)h * h);	// Assure enough space for rotation
-int wDst = 1000;
+int wDst = 2 * w;
 int hDst = wDst;
 
 void ImgRotation(unsigned char* oriBuff, unsigned char* dstBuff, double degree) {
@@ -16,16 +16,39 @@ void ImgRotation(unsigned char* oriBuff, unsigned char* dstBuff, double degree) 
 	for (int i = 0; i < hDst; i++) {	// i for yDst (y')
 		for (int j = 0; j < wDst; j++) {	// j for xDst (x')
 			/* Compute the coordinates corresponding to each pixel in the destination buffer */
-			//int xOri = (double)j * cos(rad) - (double)i * sin(rad);
-			//int yOri = (double)j * sin(rad) + (double)i * cos(rad);
-			int xOri = double(j - wDst / 2) * cos(rad) - double(i - hDst / 2) * sin(rad) + w / 2;
-			int yOri = double(j - wDst / 2) * sin(rad) + double(i - hDst / 2) * cos(rad) + h / 2;
+			double xOri = double(j - wDst / 2) * cos(rad) - double(i - hDst / 2) * sin(rad) + w / 2;
+			double yOri = double(j - wDst / 2) * sin(rad) + double(i - hDst / 2) * cos(rad) + h / 2;
+			int xOriInt = int(xOri);
+			int yOriInt = int(yOri);
 
-			/* Assignment */
-			if (xOri >= 0 && xOri <= w && yOri >=0 && yOri <= h) {
-				dstBuff[i * wDst + j] = oriBuff[yOri * w + xOri];
+			/* Assignment & bilinear interplotation */
+			if (xOriInt >= 0 && xOriInt <= w && yOriInt >=0 && yOriInt <= h) {
+				dstBuff[i * wDst + j] 
+					= oriBuff[yOriInt * w + xOriInt]           * (xOriInt + 1 - xOri) * (yOriInt + 1 - yOri)
+					+ oriBuff[yOriInt * w + xOriInt + 1]       * (xOri - xOriInt)     * (yOriInt + 1 - yOri)
+					+ oriBuff[(yOriInt + 1) * w + xOriInt]     * (xOriInt + 1 - xOri) * (yOri - yOriInt)
+					+ oriBuff[(yOriInt + 1) * w + xOriInt + 1] * (xOri - xOriInt)     * (yOri - yOriInt);
 			}
 		}
 	}
-
 }
+
+//void ImgRotation(unsigned char* oriBuff, unsigned char* dstBuff, double degree) {
+//	double rad = degree * PI / 180;	// Rotation angle in degree (clockwise)
+//	memset(dstBuff, 0, wDst * hDst * sizeof(unsigned char));	// Initialisation
+//
+//	for (int i = 0; i < hDst; i++) {	// i for yDst (y')
+//		for (int j = 0; j < wDst; j++) {	// j for xDst (x')
+//			/* Compute the coordinates corresponding to each pixel in the destination buffer */
+//			//int xOri = (double)j * cos(rad) - (double)i * sin(rad);
+//			//int yOri = (double)j * sin(rad) + (double)i * cos(rad);
+//			int xOri = double(j - wDst / 2) * cos(rad) - double(i - hDst / 2) * sin(rad) + w / 2;
+//			int yOri = double(j - wDst / 2) * sin(rad) + double(i - hDst / 2) * cos(rad) + h / 2;
+//
+//			/* Assignment */
+//			if (xOri >= 0 && xOri <= w && yOri >= 0 && yOri <= h) {
+//				dstBuff[i * wDst + j] = oriBuff[yOri * w + xOri];
+//			}
+//		}
+//	}
+//}
--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 2020.5.8 23:00
 
 - Can rotate an image by any given degree
+
+## v1.1
+
+2020.5.9 0:30
+
+- Applies bilinear interpolation when assigning pixel values to the destination buffer rather than direct assignment
